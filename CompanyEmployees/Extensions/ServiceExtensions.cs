6ffa06302847a45e5cf09b4e6eab287036d20af0
Himanshu Rawat <himanshu.g.rawat@gmail.com>
2025-04-19 21:43:02 +0530

    Configure Logger Service

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Contracts;
+using LoggerService;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CompanyEmployees.Extensions
@@ -16,5 +18,8 @@ namespace CompanyEmployees.Extensions
             services.Configure<IISOptions>(options =>
             {
             });
+
+        public static void ConfigureLoggerService(this IServiceCollection services) => 
+            services.AddScoped<ILoggerManager, LoggerManager>();
     }
 }
